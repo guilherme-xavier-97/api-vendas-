ff9d5cdb64f4a216683d97b39bf85c95a5b7bff5
Guilherme Xavier <guilhermexavier2007@hotmail.com>
2021-11-27 01:27:30 -0300

    Falha do typeorm na criação da entidade orders, nessa versão eu exclui e vou tentar de novo depois

--- src/modules/products/typeorm/entities/Product.ts
+++ src/modules/products/typeorm/entities/Product.ts
@@ -1,11 +1,9 @@
-import OrdersProducts from '@modules/orders/typeorm/entitites/OrdersProducts';
 import {
   Column,
   CreateDateColumn,
   Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
-  OneToMany,
 } from 'typeorm';
 
 @Entity('products')
@@ -27,12 +25,6 @@ class Product {
 
   @UpdateDateColumn()
   updated_at: Date;
-
-  /**o atributo order_products é have estrangeira da tabela OrdersProducts. A relação é de um para muitos,
-   * já que um produto pode estar em vários orders.
-   */
-  @OneToMany(() => OrdersProducts, order_products => order_products.product)
-  order_products: OrdersProducts[];
 }
 
 export default Product;
